2db8fcdff7a9e3ce54d4188caaa69ac68f3f9f37
CalilQ <calil_queiroz@hotmail.com>
2018-07-11 08:41:15 -0300

    Include simulation start to scripts/collect_results.sh

--- scripts/collect_results.sh
+++ scripts/collect_results.sh
@@ -1,3 +1,5 @@
-scp -i ~/.ssh/MyPCKey.pem ubuntu@ec2-18-228-10-119.sa-east-1.compute.amazonaws.com:/home/ubuntu/FD-SHARC/sharc/antenna/beamforming_normalization.tar.gz C:/Users/Calil/OneDrive/SHARC/Results/2018_06_29_vanilla_merge_test
+scp -i ~/.ssh/MyPCKey.pem ubuntu@ec2-18-228-10-119.sa-east-1.compute.amazonaws.com:/home/ubuntu/FD-SHARC/scripts/simulation_resutls.tar.gz C:/Users/Calil/OneDrive/SHARC
 
-scp -i ~/.ssh/MyPCKey.pem C:/Users/Calil/OneDrive/SHARC/Results/2018_06_29_vanilla_merge_test/Parameters/Parameters.zip ubuntu@ec2-18-228-10-119.sa-east-1.compute.amazonaws.com:/home/ubuntu/FD-SHARC/cases
+scp -i ~/.ssh/MyPCKey.pem C:/Users/Calil/OneDrive/SHARC/Results/2018_06_30_phase_2_ES/parameters/parameters_directories/parameters_directories.zip ubuntu@ec2-18-228-10-119.sa-east-1.compute.amazonaws.com:/home/ubuntu/FD-SHARC/cases
+
+nohup bash -x simulate_all.sh > simulate_all.out 2>&1 &
