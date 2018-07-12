fe1302dd7019ecd80ec1e20d16c0340b16c86f43
CalilQ <calil_queiroz@hotmail.com>
2018-07-12 10:07:59 -0300

    Improve collect_results.sh script

--- scripts/collect_results.sh
+++ scripts/collect_results.sh
@@ -4,4 +4,8 @@ scp -i ~/.ssh/MyPCKey.pem C:/Users/Calil/OneDrive/SHARC/Results/2018_06_30_phase
 
 nohup bash -x simulate_all.sh > simulate_all.out 2>&1 &
 
-scp C:/Users/Calil/OneDrive/SHARC/Results/2018_06_30_phase_2_ES/campaigns/campaign_1/parameters/cases_phase2_campaign1.zip calil@172.16.20.76:/home/calil/FD-SHARC/cases
+scp C:/Users/Calil/OneDrive/SHARC/Results/2018_06_30_phase_2_ES/campaigns/campaign_3/parameters/cases_phase2_campaign3.zip calil@172.16.20.76:/home/calil/FD-SHARC/cases
+
+scp calil@172.16.20.76:/home/calil/FD-SHARC/results.tar.gz C:/Users/Calil/OneDrive/SHARC/Results/2018_06_30_phase_2_ES/campaigns/campaign_1
+
+scp C:/Users/Calil/OneDrive/SHARC/Results/2018_06_30_phase_2_ES/campaigns/campaign_2/parameters/parameters_phase2_campaign2.zip calil@172.16.20.76:/home/calil/FD-SHARC/sharc/parameters
