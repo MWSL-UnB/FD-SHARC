1fae4e66d04deed98fc3775130d7d00527f0fd36
CalilQ <calil_queiroz@hotmail.com>
2018-09-11 09:52:02 -0300

    Create and test AntennaOmniBeam class
    Class is supposed to be used in beam assignment testing

---
+++ sharc/antenna/antenna_omni_beam.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 11 09:06:55 2018
+
+@author: Calil
+"""
+
+import numpy as np
+
+from sharc.antenna.antenna import Antenna
+
+
+class AntennaOmniBeam(Antenna):
+    def __init__(self,):
+        super().__init__()
+
+    def add_beam(self, phi: float, theta: float):
+        self.beams_list.append((phi,theta-90))
+
+    def calculate_gain(self, *args, **kwargs) -> np.array:
+        phi_vec = np.asarray(kwargs["phi_vec"])
+        theta_vec = np.asarray(kwargs["theta_vec"])
+        if("beams_l" in kwargs.keys()): 
+            beams_l = np.asarray(kwargs["beams_l"],dtype=int)
+        else: 
+            beams_l = -1*np.ones_like(phi_vec)
+           
+        n_direct = len(theta_vec)
+
+        gains = np.zeros(n_direct)
+
+        for g in range(n_direct):
+            self.beams_list[beams_l[g]]
+            gains[g] = beams_l[g]
+
+        return gains
+
+    def reset_beams(self):
+        self.beams_list = []
+
---
+++ tests/test_antenna_omni_beam.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 11 09:22:17 2018
+
+@author: Calil
+"""
+
+import numpy as np
+
+import unittest
+import numpy.testing as npt
+
+from sharc.antenna.antenna_omni_beam import AntennaOmniBeam
+
+class AntennaOmniBeamTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.antenna = AntennaOmniBeam()
+        
+    def test_calculate_gain(self):
+        # Add a beam
+        self.antenna.add_beam( 35.0, 45.0)
+        self.antenna.add_beam(-35.0, 25.0)
+        self.antenna.add_beam( 90.0,-40.0)
+        
+        # Create angles
+        num_angles = 5
+        phi_vec = np.ones(num_angles)
+        theta_vec = np.ones(num_angles)
+        beams_idx = np.array([0,1,2,1,0])
+        
+        # Calculate gains
+        gains = self.antenna.calculate_gain(phi_vec=phi_vec,
+                                            theta_vec=theta_vec,
+                                            beams_l=beams_idx)
+        
+        # Assert
+        npt.assert_equal(gains,beams_idx)
+        
+        # Check error raise
+        beams_idx = np.array([0,1,3,1,0])
+        
+        # Calculate gains
+        with self.assertRaises(IndexError):
+            gains = self.antenna.calculate_gain(phi_vec=phi_vec,
+                                                theta_vec=theta_vec,
+                                                beams_l=beams_idx)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
