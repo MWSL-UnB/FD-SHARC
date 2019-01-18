35e3a04b264a8b8c3de3fcc2e21b788c261c6f1e
CalilQ <calil_queiroz@hotmail.com>
2019-01-18 16:47:20 -0200

    Fix bugs in IMT P.452 propagation

--- sharc/propagation/propagation_imt_p452.py
+++ sharc/propagation/propagation_imt_p452.py
@@ -17,24 +17,34 @@ class PropagationImtP452(Propagation):
         num_sec = kwargs.pop("number_of_sectors", 1)
         indoor_stas= kwargs.pop("indoor_stations", 1)
         ele = kwargs["elevation"]
-        bel = kwargs.pop("bel_enabled", True)
+        bel = kwargs.pop("bel_enabled", False)
         es_par = kwargs["es_params"]
-        tx_g = np.ravel(np.asarray(kwargs["tx_gain"]))
-        rx_g = np.ravel(np.asarray(kwargs["rx_gain"]))
+        tx_g = kwargs["tx_gain"]
+        rx_g = kwargs["rx_gain"]
 
-        loss = self.p452.get_loss(distance_3D=distance,
-                                  frequency=f,
-                                  number_of_sectors=num_sec,
-                                  indoor_stations=indoor_stas,
-                                  elevation=ele,
-                                  bel_enabled=bel,
-                                  es_params=es_par,
-                                  tx_gain=tx_g,
-                                  rx_gain=rx_g)
+        loss_mtx = np.zeros_like(distance)
+
+        distance[np.isnan(distance)] = 100.0
+        tx_g[np.isnan(tx_g)] = 0.0
+        rx_g[np.isnan(rx_g)] = 0.0
+
+        for k, dist in enumerate(distance):
+            k_range = [m for m in range(3 * (k // 3), 3 * (k // 3) + 3)]
+            loss = self.p452.get_loss(distance_3D=np.array([dist]),
+                                      frequency=np.array([f[k]]),
+                                      number_of_sectors=num_sec,
+                                      indoor_stations=np.array([indoor_stas[k]]),
+                                      elevation=ele,
+                                      bel_enabled=bel,
+                                      es_params=es_par,
+                                      tx_gain=tx_g[k,:],
+                                      rx_gain=np.reshape(rx_g[:,k_range], (171)))
+
+            loss_mtx[k,:] = loss
 
         for k, ls in enumerate(loss):
             idx_range = [3 * (k // 3) + m for m in range(3)]
-            loss[k, idx_range] = es_par.co_site_bs_loss
-            loss[k, k] = np.nan
+            loss_mtx[k, idx_range] = es_par.co_site_bs_loss
+            loss_mtx[k, k] = np.nan
 
-        return loss
+        return loss_mtx
